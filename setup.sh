cf17bcdb9af41bf48780fc0809442b6ebb8f1b48
Amar Mesic <amar.s.mesic@gmail.com>
2026-03-07 12:19:33 +0100

    change docker command to be supported in linux

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 # Start production-style Docker Compose stack.
 # Assumes a valid .env file already exists in the repo root.
 
-docker compose -f docker-compose.prod.yml build
-docker compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml up -d
